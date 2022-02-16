7870a7de4f2985476244aa9a30139a6e7ab229fc
Limpch <romanlipka2612@gmail.com>
2022-02-16 18:54:25 +0300

    job

--- src/components/Home page/Options/Options.jsx
+++ src/components/Home page/Options/Options.jsx
@@ -1,21 +1,42 @@
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 import classes from './Options.module.scss'
 
 export const Options = () => {
+	
+	const [isActiveFirst, setActiveFirst] = useState('')
+	const [isActiveSecond, setActiveSecond] = useState('')
+	const [langText, setLangText] = useState('RU')
+
   return (
 	 <div className={classes.Options}>
 		 <h3>Settings</h3>
 		 <ul>
-			<li><motion.span
-			 className={classes.Theme}
-			 animate={{
-				background: 'linear-gradient(90deg, var(--main-clr) 0%,  var(--main-clr) 50%,  var(--bg-container-clr) 50%,  var(--bg-container-clr) 100%)',
-			 }}
-			 whileFocus={{
-				background: 'var(--main-clr)',
-			 }}
-			 ></motion.span><p>Change theme</p></li>
-			<li><span className={classes.Lang}>RU</span><p>Change language</p></li>
+			<li onMouseEnter={() => setActiveFirst(classes.active)} onMouseLeave={() => setActiveFirst('')}><span className={classes.Theme}>
+			<motion.span
+				initial={{left: '-200%'}}
+				whileHover={{left: '0%'}}
+			></motion.span>
+			</span><p className={isActiveFirst}>Change theme</p></li>
+			<li 
+			onMouseEnter={() => setActiveSecond(classes.active)} 
+			onMouseLeave={() => setActiveSecond('')}
+			>
+				<motion.span 
+				className={classes.Lang}
+				transition={{
+					duration: 0.6
+				}}
+				whileHover={{
+					translateX: [0,4,-4,4,-4,4,-4,0]
+				}}
+				onMouseEnter={() => setTimeout(() => {setLangText('ENG')},300)}
+				onMouseLeave	={() => setLangText('RU')}
+				>
+				{langText}
+				</motion.span>
+				<p className={isActiveSecond}>Change language</p>
+			</li>
 		 </ul>
 	 </div>
   )
